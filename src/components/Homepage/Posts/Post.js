84aae5a72e7769074b75fc7d7d211bc44e40062f
joyceyskwon <joyceyskwon@gmail.com>
2021-02-26 21:58:53 -0500

    added checkboxes to Posts

--- src/components/Homepage/Posts/Post.js
+++ src/components/Homepage/Posts/Post.js
@@ -9,33 +9,15 @@ export const Post = props => {
       className="post grid-item"
     >
       <h3>{post.author}</h3>
-      <p>Created: {post.creationDateTime}</p>
-      <p>Yesterday:</p>
+      <p>{post.creationDateTime}</p>
+      {/* <p>Yesterday:</p> */}
       <ul>
         {post.yesterday.map((yesterday, index) => {
           return(
             <li key={index}>
-             {yesterday} 
-            </li>
-          )
-        })}
-      </ul>
-      <p>Today:</p>
-      <ul>
-        {post.today.map((today, index) => {
-          return (
-            <li key={index}>
-              {today}
-            </li>
-          )
-        })}
-      </ul>
-      <p>What is stopping you?</p>
-      <ul>
-        {post.obstacle.map((obstacle, index) => {
-          return (
-            <li key={index}>
-              {obstacle}
+              <input type="checkbox">
+              </input>
+              <label>{yesterday}</label>
             </li>
           )
         })}
